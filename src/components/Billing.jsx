665cc3d306b56e77ff2ea9cbd58c21963ba67033
derick-asane <derickasane@gmail.com>
2024-07-01 18:15:48 +0100

    started the billing

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,17 @@
-import React from "react";
-
-const Billing = () => {
-  return <div></div>;
-};
+import { apple, bill, google } from "../assets";
+import { layout } from "../style";
+const Billing = () => (
+  <section className={layout.sectionReverse} id="product">
+    <div className={layout.sectionImgReverse}>
+      <img
+        src={bill}
+        alt="billing"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
+      <div className="absolte z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolte z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+    </div>
+  </section>
+);
 
 export default Billing;

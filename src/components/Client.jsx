27d03212223f30db9fab36edd7027eee10fc1049
derick-asane <derickasane@gmail.com>
2024-07-02 10:31:02 +0100

    we did the cta, client, footer , testimonial, Billing. end of project

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "../style";
+import { clients } from "../constants";
 
-function Client() {
-  return <div></div>;
-}
+const Client = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+        >
+          <img
+            src={client.logo}
+            alt="client"
+            className="sm:w-[192px] w-[100px] object-contain hover:bg-slate-50 p-4"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Client;
